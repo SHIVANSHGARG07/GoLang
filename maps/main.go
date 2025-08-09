d7c91713f72e5e8e13201fcf4686976796bb90e1
shivanshgarg07 <shivanshgarg587@gmail.com>
2025-08-10 01:14:08 +0530

    Maps

---
+++ maps/main.go
@@ -0,0 +1,31 @@
+// unordered collection of data key value [pairs]
+
+package main
+
+import "fmt"
+
+func main() {
+	m1 := make(map[string]int)
+	m2 := make(map[string]string)
+
+	m1["user1"] = 10
+	m2["user1"] = "first"
+	m1["user2"] = 20
+	m2["user2"] = "second"
+
+	fmt.Println("u1", m1["user1"], "id", m2["user1"])
+	fmt.Println("u2", m1["user2"], "id", m2["user2"])
+
+	delete(m1, "user1")
+	// fmt.Println("u1", m1["user1"], "id", m2["user1"])
+
+	// this map always return two values
+	// firstly the value and second one is boolean nature
+	marks, exist := m1["user1"]
+	if exist {
+		fmt.Println(marks)
+	} else {
+		fmt.Println("no user found for this")
+	}
+
+}
